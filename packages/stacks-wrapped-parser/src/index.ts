b55c283ac5425fd9fa43b78e0f9cfaf41d45ecc3
winsznx <timjosh507@gmail.com>
2026-04-12 14:48:56 +0100

    add parser package entry point with computeWrappedStats orchestrator

---
+++ packages/stacks-wrapped-parser/src/index.ts
@@ -0,0 +1,24 @@
+export { RawTransaction, WrappedStats } from "./types";
+export { microToSTX } from "./utils/microToSTX";
+export { computeTotalTransactions } from "./parsers/computeTotalTransactions";
+export { computeTotalFeesPaid } from "./parsers/computeTotalFeesPaid";
+export { computeFirstTransactionDate } from "./parsers/computeFirstTransactionDate";
+export { computeFavoriteContract } from "./parsers/computeFavoriteContract";
+export { computeBiggestSTXTransfer } from "./parsers/computeBiggestSTXTransfer";
+
+import { RawTransaction, WrappedStats } from "./types";
+import { computeTotalTransactions } from "./parsers/computeTotalTransactions";
+import { computeTotalFeesPaid } from "./parsers/computeTotalFeesPaid";
+import { computeFirstTransactionDate } from "./parsers/computeFirstTransactionDate";
+import { computeFavoriteContract } from "./parsers/computeFavoriteContract";
+import { computeBiggestSTXTransfer } from "./parsers/computeBiggestSTXTransfer";
+
+export function computeWrappedStats(txs: RawTransaction[]): WrappedStats {
+  return {
+    totalTransactions: computeTotalTransactions(txs),
+    totalFeesPaidSTX: computeTotalFeesPaid(txs),
+    firstTransactionDate: computeFirstTransactionDate(txs),
+    favoriteContract: computeFavoriteContract(txs),
+    biggestSTXTransferSTX: computeBiggestSTXTransfer(txs),
+  };
+}
