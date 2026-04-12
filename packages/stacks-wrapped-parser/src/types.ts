1911971fb8811615ba8fa0a23628b7709ed708a9
winsznx <timjosh507@gmail.com>
2026-04-12 14:48:15 +0100

    define RawTransaction and WrappedStats types

---
+++ packages/stacks-wrapped-parser/src/types.ts
@@ -0,0 +1,23 @@
+export interface RawTransaction {
+  tx_id: string;
+  tx_type: string;
+  burn_block_time_iso: string;
+  fee_rate: string;
+  sender_address: string;
+  token_transfer?: {
+    recipient_address: string;
+    amount: string;
+  };
+  contract_call?: {
+    contract_id: string;
+    function_name: string;
+  };
+}
+
+export interface WrappedStats {
+  totalTransactions: number;
+  totalFeesPaidSTX: number;
+  firstTransactionDate: string;
+  favoriteContract: string;
+  biggestSTXTransferSTX: number;
+}
