4ed16024864b6ed605433bc1f7da1fa451b1a216
winsznx <timjosh507@gmail.com>
2026-04-14 18:18:37 +0100

    add formatSTX utility for locale-aware STX formatting

--- packages/stacks-wrapped-parser/src/utils/microToSTX.ts
+++ packages/stacks-wrapped-parser/src/utils/microToSTX.ts
@@ -1,3 +1,10 @@
 export function microToSTX(microSTX: string | number): number {
   return Number(microSTX) / 1_000_000;
 }
+
+export function formatSTX(stx: number): string {
+  return stx.toLocaleString("en-US", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 6,
+  });
+}
