27bfd712a88ccd5db27407eaeca3854f98de9333
winsznx <timjosh507@gmail.com>
2026-04-14 18:18:37 +0100

    add array guard to computeTotalTransactions

--- packages/stacks-wrapped-parser/src/parsers/computeTotalTransactions.ts
+++ packages/stacks-wrapped-parser/src/parsers/computeTotalTransactions.ts
@@ -1,5 +1,6 @@
 import { RawTransaction } from "../types";
 
 export function computeTotalTransactions(txs: RawTransaction[]): number {
+  if (!Array.isArray(txs)) return 0;
   return txs.length;
 }
