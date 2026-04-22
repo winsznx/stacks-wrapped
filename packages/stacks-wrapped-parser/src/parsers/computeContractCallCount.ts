599890880f7caa1dbfbb41cafc4f058248ae3b9c
winsznx <timjosh507@gmail.com>
2026-04-22 15:16:57 +0000

    fix(parser): only count successful contract calls

--- packages/stacks-wrapped-parser/src/parsers/computeContractCallCount.ts
+++ packages/stacks-wrapped-parser/src/parsers/computeContractCallCount.ts
@@ -2,5 +2,5 @@ import { RawTransaction } from "../types";
 import { isContractCall } from "../guards/isContractCall";
 
 export function computeContractCallCount(txs: RawTransaction[]): number {
-  return txs.filter(isContractCall).length;
+  return txs.filter((tx) => isContractCall(tx) && tx.tx_status === "success").length;
 }
