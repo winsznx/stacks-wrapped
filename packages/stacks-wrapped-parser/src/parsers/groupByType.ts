13187d75f6b484224c158dec3f35e71101d86d56
winsznx <timjosh507@gmail.com>
2026-04-22 12:34:39 +0000

    refactor(parser): optimize groupByType with reduce

--- packages/stacks-wrapped-parser/src/parsers/groupByType.ts
+++ packages/stacks-wrapped-parser/src/parsers/groupByType.ts
@@ -1,10 +1,12 @@
 import { RawTransaction } from "../types";
 
 export function groupByType(txs: RawTransaction[]): Record<string, RawTransaction[]> {
-  const groups: Record<string, RawTransaction[]> = {};
-  for (const tx of txs) {
-    if (!groups[tx.tx_type]) groups[tx.tx_type] = [];
-    groups[tx.tx_type].push(tx);
-  }
-  return groups;
+  return txs.reduce((groups, tx) => {
+    const { tx_type } = tx;
+    if (!groups[tx_type]) {
+      groups[tx_type] = [];
+    }
+    groups[tx_type].push(tx);
+    return groups;
+  }, {} as Record<string, RawTransaction[]>);
 }
