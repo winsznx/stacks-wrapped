c3de2e1301872f5ddc3eb05f969d139e22219f09
winsznx <timjosh507@gmail.com>
2026-04-22 11:26:52 +0000

    refactor(parser): use Set for unique contract counting

--- packages/stacks-wrapped-parser/src/parsers/computeUniqueContractsCount.ts
+++ packages/stacks-wrapped-parser/src/parsers/computeUniqueContractsCount.ts
@@ -2,11 +2,11 @@ import { RawTransaction } from "../types";
 import { isContractCall } from "../guards/isContractCall";
 
 export function computeUniqueContractsCount(txs: RawTransaction[]): number {
-  const unique = new Set<string>();
+  const contracts = new Set<string>();
   for (const tx of txs) {
     if (isContractCall(tx)) {
-      unique.add(tx.contract_call.contract_id);
+      contracts.add(tx.contract_call.contract_id);
     }
   }
-  return unique.size;
+  return contracts.size;
 }
