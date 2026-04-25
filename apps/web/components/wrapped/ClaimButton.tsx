1c938f0d19d73dfc6cc47674ecc23b218d39a78f
winsznx <timjosh507@gmail.com>
2026-04-25 23:54:04 +0000

    refactor(web): update ClaimButton for v7 post-condition patterns

--- apps/web/components/wrapped/ClaimButton.tsx
+++ apps/web/components/wrapped/ClaimButton.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react";
 import { openContractCall } from "@stacks/connect";
+import { PostConditionMode } from "@stacks/transactions";
 import { StacksMainnet } from "@stacks/network";
 import { CONTRACT_DEPLOYER, CONTRACT_NAME } from "@/lib/constants";
 
