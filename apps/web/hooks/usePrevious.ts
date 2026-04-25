fe338ce33847bcda3952cc15aad577aa059cd2c2
winsznx <timjosh507@gmail.com>
2026-04-25 08:58:30 +0000

    refactor(web): add strict generic typing to usePrevious

--- apps/web/hooks/usePrevious.ts
+++ apps/web/hooks/usePrevious.ts
@@ -1,11 +1,12 @@
 "use client";
-
-import { useEffect, useRef } from "react";
+import { useRef, useEffect } from "react";
 
 export function usePrevious<T>(value: T): T | undefined {
   const ref = useRef<T | undefined>(undefined);
+
   useEffect(() => {
     ref.current = value;
   }, [value]);
+
   return ref.current;
 }
