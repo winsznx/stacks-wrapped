e988dafaeac7252ba5d04206dd739f622266e11c
winsznx <timjosh507@gmail.com>
2026-04-26 04:52:17 +0100

    fix(web): use style prop for Skeleton dimensions

--- apps/web/app/leaderboard/loading.tsx
+++ apps/web/app/leaderboard/loading.tsx
@@ -3,9 +3,9 @@ import { Skeleton } from "@/components/ui/Skeleton";
 export default function LeaderboardLoading() {
   return (
     <main className="leaderboard-page">
-      <Skeleton height={48} width="30%" />
-      <Skeleton height={200} />
-      <Skeleton height={320} />
+      <Skeleton style={{ height: 48, width: "30%" }} />
+      <Skeleton style={{ height: 200 }} />
+      <Skeleton style={{ height: 320 }} />
     </main>
   );
 }
