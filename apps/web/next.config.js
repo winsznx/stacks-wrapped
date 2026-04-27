a2fa173fc8dc3f3024ca953f4c1b0b4599a8b809
winsznx <timjosh507@gmail.com>
2026-04-27 06:06:22 +0000

    chore(web): add JSDoc type annotation to next.config.js

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,5 @@
 /** @type {import('next').NextConfig} */
+/** @type {import("next").NextConfig} */
 const nextConfig = {
   transpilePackages: ["@winsznx/stacks-wrapped-parser"],
   poweredByHeader: false,
